b8f7389fb361b403289f2aa864b4cabb239422b8
Iaggelis <john_agelis@hotmail.com>
2018-05-13 23:14:34 +0300

    Added transpose and map functions

--- Matrix.h
+++ Matrix.h
@@ -1,37 +1,58 @@
 #include <vector>
 #include <iterator>
 #include <iostream>
+#include <iomanip>
 #include <random>
 #include <algorithm>
+#include <functional>
 
 template <typename T>
 class Matrix
 {
+  private:
   public:
-    Matrix(size_t rows, size_t columns)
+    size_t mRows, mColumns, mSize;
+    std::vector<T> mData;
+    Matrix()
+        : mRows(0), mColumns(0), mSize(0)
+    {
+    }
+
+    Matrix(const size_t &rows, const size_t &columns)
         : mRows(rows), mColumns(columns), mSize(columns * rows)
     {
         mData.reserve(mSize);
     }
+
+    Matrix(const size_t &rows, const size_t &columns, const std::vector<T> &Data)
+        : mRows(rows), mColumns(columns), mSize(columns * rows), mData(Data)
+    {
+    }
+
     Matrix(const Matrix &m)
-        : mRows{m.rows}, mColumns{m.columns}, mSize{m.getSize()}, mData{m.getData()}
     {
+        // *this = m;
+        mRows = m.rows();
+        mColumns = m.columns();
+        mSize = mRows * mColumns;
+        mData = m.mData;
     }
 
-    void add(const T val)
-    {
-        mData.push_back(val);
-    }
+    // Matrix(Matrix &&m) noexcept
+    //     : mRows(m.rows),
+    //       mColumns(m.columns)
+    // {
+    //     mSize = mRows * mColumns;
+    //     mData = m.getData();
+    // }
 
-    T operator[](size_t i)
+    ~Matrix() noexcept
     {
-        return mData.at(i);
-    }
-
-    T operator()(int i, int j)
-    {
-        // return mData[i*mRows + j];
-        return mData.at(j + i * mColumns);
+        mRows = 0;
+        mColumns = 0;
+        mSize = 0;
+        mData.clear();
+        mData.shrink_to_fit();
     }
 
     void randomize()
@@ -39,12 +60,17 @@ class Matrix
         std::random_device r;
         std::seed_seq seed2{r(), r(), r(), r(), r(), r(), r(), r()};
         std::mt19937 e2(seed2);
-        std::normal_distribution<> normal_dist(0, 1);
+        std::normal_distribution<> normal_dist(5, 3.24);
         for (int i = 0; i < mSize; i++)
         {
             mData.push_back(normal_dist(e2));
             // x = normal_dist(e2);
         }
+    }
+
+    void insert(const T &val)
+    {
+        mData.push_back(val);
     }
 
     void add(const T &val)
@@ -57,7 +83,49 @@ class Matrix
 
     void add(const Matrix &m)
     {
-        mData += m.mData;
+        std::transform(mData.begin(), mData.end(), m.mData.begin(), mData.begin(), std::plus<T>());
+        // mData += m.getData();
+    }
+
+    void subtrack(const Matrix &m)
+    {
+        std::transform(mData.begin(), mData.end(), m.mData.begin(), mData.begin(), std::minus<T>());
+    }
+
+    void transpose()
+    {
+        Matrix<T> temp(mColumns, mRows);
+        std::vector<T> tempv;
+        for (int i = 0; i < mColumns; i++)
+        {
+            for (int j = 0; j < mRows; j++)
+            {
+                temp.insert((*this)(j, i));
+                tempv.push_back((*this)(j, i));
+            }
+        }
+        // *this = temp;
+        std::swap(mColumns, mRows);
+        mData.clear();
+        mData = tempv;
+    }
+
+    void map(std::function<T(T)> &f)
+    {
+        for (auto &data : mData)
+        {
+            data = f(data);
+        }
+    }
+
+    Matrix addMatrix(Matrix &m)
+    {
+        return Matrix(4, 4, (mData));
+    }
+
+    Matrix copy()
+    {
+        return *this;
     }
 
     void multiply(const T &val)
@@ -73,34 +141,74 @@ class Matrix
         return mData;
     }
 
-    // Matrix operator+(const T val){
-    // 	this->add(val)
-    // 	return this;
-    // }
-
     void print() const
     {
         for (int i = 0; i < mRows; i++)
         {
             for (int j = 0; j < mColumns; j++)
             {
-                std::cout << (*this)(i, j) << "\t";
+                std::cout << std::setw(11) << (*this)(i, j) << std::setw(11);
             }
             std::cout << "\n";
         }
+        std::cout << "------------------------------------------------------------------------------ \n";
     }
 
-    T row(size_t i) const
+    size_t rows() const
+    {
+        return mRows;
+    }
+
+    size_t columns() const
+    {
+        return mColumns;
+    }
+    size_t getSize() const
+    {
+        // return mSize;
+        return mData.size();
+    }
+
+    T operator[](size_t i) const
     {
         return mData.at(i);
     }
 
-    int getSize() const
+    T operator()(int i, int j) const
     {
-        return mSize;
+        // return mData[i*mRows + j];
+        return mData.at(j + i * mColumns);
     }
 
-  private:
-    size_t mRows, mColumns, mSize;
-    std::vector<T> mData;
+    Matrix &operator=(const Matrix &x)
+    {
+        if (this == &x)
+        {
+            return *this;
+        }
+        // if (mSize != x.getSize())
+        // {
+        //     return *this;
+        // }
+        // mData.clear();
+        mData.resize(x.mSize);
+        mData = x.mData;
+        // std::cout << mData.size() << "\n";
+
+        // std::swap(mData, x.mData);
+        return *this;
+    }
+
+    // Matrix operator+(const Matrix &x)
+    // {
+    //     if (mSize != x.getSize())
+    //     {
+    //         std::cout << "Wrong Matrix size!"
+    //                   << "\n";
+    //         return *this;
+    //     }
+    //     // std::vector<T> new_vec = this->mData + x.getData();
+
+    //     return Matrix<T>(mRows, mColumns, mData);
+    // }
 };
--- example.cpp
+++ example.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 #include <vector>
-#include <Matrix.h>
+#include <functional>
+#include <cmath>
+#include "Matrix.h"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    Matrix<double> matrix(4, 4);
+    Matrix<double> matrix1(3, 2), matrix2(2, 2);
     // cout << matrix.getSize() << endl;
-    matrix.randomize();
-    // matrix.add(325.0);
-    // matrix.add(1.0);
-    // matrix.add(2.0);
-    // matrix.add(3.0);
-    matrix.print();    
-    // cout << matrix.row(0) << endl;
+    matrix1.randomize();
+    matrix2.randomize();
+    matrix1.print();
+    // matrix2.print();
+    cout << '\n';
+
+    function<double(double)> ayy = [&](double x) -> double { return x * 2; };
+    Matrix<double> matrix3 = matrix1.copy();
+    // matrix3.print();
+    // matrix3.transpose();
+    matrix3.map(ayy);
+    matrix3.print();
+    // matrix3 = matrix1.copy();
+
     return 0;
 }
